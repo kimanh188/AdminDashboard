b7ce27ffeba58335ca8793d9ed38a961f85e5e62
Kim Anh Do <kim@Kims-MBP.fritz.box>
2023-11-13 13:57:20 +0100

    Added NewsSidebar

--- app/ui/dashboard/news-sidebar-right/newsSidebar.jsx
+++ app/ui/dashboard/news-sidebar-right/newsSidebar.jsx
@@ -1,6 +1,53 @@
-import "./newsSidebar.module.css";
+import Image from "next/image";
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
+import styles from "./newsSidebar.module.css";
 
 const NewsSidebar = () => {
-  return <div>NewsSidebar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image className={styles.bg} src="/instruction.png" alt="" fill />
+        </div>
+
+        <div className={styles.text}>
+          <span className={styles.notification}>🔥 Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 5 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos labore
+            neque cum commodi consequuntur magni numquam, amet qui dolores
+            reiciendis.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled />
+            Watch
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.text}>
+          <span className={styles.notification}>🚀 Coming Soon</span>
+          <h3 className={styles.title}>
+            New server actions are available, partial pre-rendering is coming
+            up!
+          </h3>
+          <span className={styles.subtitle}>Boost your productivity</span>
+          <p className={styles.desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum quos
+            inventore error qui aut deserunt dignissimos atque debitis quaerat
+            cum!
+          </p>
+          <button className={styles.button}>
+            <MdReadMore />
+            Learn
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default NewsSidebar;
